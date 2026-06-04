dc7b9d1c32491fd40185de9a6b6c28aed24eb187
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2026-06-04 00:18:41 -0400

    Add files via upload

---
+++ Homework 0/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    private String name;
+    private int age;
+    private String ssn;
+    private String address;
+    public Person() {
+        name = "";
+        age = 0;
+        ssn = "";
+        address = "";
+    }
+    public Person(String name, int age, String ssn, String address) {
+        this.name = name;
+        this.age = age;
+        this.ssn = ssn;
+        this.address = address;
+    }
+    
+    
+}
---
+++ Homework 0/Statistics.java
@@ -0,0 +1,35 @@
+
+public class Statistics {
+    private int[] valss = new int[];
+    private int size = 0;
+    public Statistics() {
+        size = 0;
+    }
+
+    public Statistics(ArrayList<Integer> vals) {
+        
+    }
+    public void add(int val) {
+    }
+    public void add(ArrayList<Integer> vals) {
+
+    }
+    public int getAverage() {
+
+    }
+    public int getMaximum() {
+
+    }
+    public int getMinimum() {
+
+    }
+    public int getMedian() {
+
+    }
+    public int getRange() {
+
+    }
+    public void clear() {
+
+    }
+}
